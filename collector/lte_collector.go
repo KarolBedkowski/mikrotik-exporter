73377d3e54c10ad615bf24d639276a5b7595f731
Karol Będkowski <karol.bedkowski@gmail.com>
2024-03-21 17:03:18 +0100

    fix: fix (?) lte collector

--- collector/lte_collector.go
+++ collector/lte_collector.go
@@ -2,75 +2,65 @@ package collector
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 
-	"github.com/KarolBedkowski/routeros-go-client/proto"
 	"github.com/hashicorp/go-multierror"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// TODO: probably totally broken
+// TODO: need check
 
 func init() {
 	registerCollector("lte", newLteCollector)
 }
 
 type lteCollector struct {
-	props        []string
-	propslist    string
-	descriptions map[string]*prometheus.Desc
+	metrics propertyMetricList
 }
 
 func newLteCollector() routerOSCollector {
-	c := &lteCollector{
-		descriptions: make(map[string]*prometheus.Desc),
+	const prefix = "lte_interface"
+
+	labelNames := []string{"name", "address", "interface", "cell_id", "primary_band"}
+
+	return &lteCollector{
+		metrics: propertyMetricList{
+			newPropertyGaugeMetric(prefix, "rssi", labelNames).build(),
+			newPropertyGaugeMetric(prefix, "rsrp", labelNames).build(),
+			newPropertyGaugeMetric(prefix, "rsrq", labelNames).build(),
+			newPropertyGaugeMetric(prefix, "sinr", labelNames).build(),
+			newPropertyGaugeMetric(prefix, "status", labelNames).
+				withName("connected").withConverter(convertFromLteStatus).build(),
+		},
 	}
-
-	c.props = []string{"current-cellid", "primary-band", "ca-band", "rssi", "rsrp", "rsrq", "sinr"}
-	c.propslist = strings.Join(c.props, ",")
-	labelNames := []string{"name", "address", "interface", "cellid", "primaryband", "caband"}
-
-	for _, p := range c.props {
-		c.descriptions[p] = descriptionForPropertyName("lte_interface", p, labelNames)
-	}
-
-	return c
 }
 
 func (c *lteCollector) describe(ch chan<- *prometheus.Desc) {
-	for _, d := range c.descriptions {
-		ch <- d
-	}
+	c.metrics.describe(ch)
 }
 
 func (c *lteCollector) collect(ctx *collectorContext) error {
-	names, err := c.fetchInterfaceNames(ctx)
+	reply, err := ctx.client.Run("/interface/lte/print", "?disabled=false", "=.proplist=name")
 	if err != nil {
-		return err
+		return fmt.Errorf("fetch lte interface names error: %w", err)
 	}
 
+	names := extractPropertyFromReplay(reply, "name")
+
+	var errs *multierror.Error
+
 	for _, n := range names {
-		err := c.collectForInterface(n, ctx)
-		if err != nil {
-			return err
+		if err := c.collectForInterface(n, ctx); err != nil {
+			errs = multierror.Append(errs, err)
 		}
 	}
 
-	return nil
-}
-
-func (c *lteCollector) fetchInterfaceNames(ctx *collectorContext) ([]string, error) {
-	reply, err := ctx.client.Run("/interface/lte/print", "?disabled=false", "=.proplist=name")
-	if err != nil {
-		return nil, fmt.Errorf("fetch lte interface names error: %w", err)
-	}
-
-	return extractPropertyFromReplay(reply, "name"), nil
+	return errs.ErrorOrNil()
 }
 
 func (c *lteCollector) collectForInterface(iface string, ctx *collectorContext) error {
-	reply, err := ctx.client.Run("/interface/lte/info", "=number="+iface, "=once=", "=.proplist="+c.propslist)
+	reply, err := ctx.client.Run("/interface/lte/monitor", "=number="+iface, "=once=",
+		"=.proplist=current-cellid,primary-band,rssi,rsrp,rsrq,sinr,status")
 	if err != nil {
 		return fmt.Errorf("fetch %s lte interface statistics error: %w", iface, err)
 	}
@@ -79,48 +69,26 @@ func (c *lteCollector) collectForInterface(iface string, ctx *collectorContext) 
 		return nil
 	}
 
-	var errs *multierror.Error
+	re := reply.Re[0]
 
-	for _, p := range c.props[3:] {
-		// there's always going to be only one sentence in reply, as we
-		// have to explicitly specify the interface
-		if err := c.collectMetricForProperty(p, iface, reply.Re[0], ctx); err != nil {
-			errs = multierror.Append(errs, fmt.Errorf("collect %s for %s error: %w", p, iface, err))
-		}
-	}
-
-	return errs.ErrorOrNil()
-}
-
-func (c *lteCollector) collectMetricForProperty(property, iface string,
-	reply *proto.Sentence, ctx *collectorContext,
-) error {
-	desc := c.descriptions[property]
-	currentCellID := reply.Map["current-cellid"]
-	// get only band and its width, drop earfcn and phy-cellid info
-	primaryband := reply.Map["primary-band"]
+	primaryband := re.Map["primary-band"]
 	if primaryband != "" {
 		primaryband = strings.Fields(primaryband)[0]
 	}
 
-	caband := reply.Map["ca-band"]
-	if caband != "" {
-		caband = strings.Fields(caband)[0]
-	}
+	ctx = ctx.withLabels(iface, re.Map["current-cellid"], primaryband)
 
-	value := reply.Map[property]
-	if value == "" {
-		return nil
+	if err := c.metrics.collect(re, ctx); err != nil {
+		return fmt.Errorf("collect ltr for %s error: %w", iface, err)
 	}
-
-	v, err := strconv.ParseFloat(value, 64)
-	if err != nil {
-		return fmt.Errorf("parse %v error: %w", value, err)
-	}
-
-	ctx.ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, v,
-		ctx.device.Name, ctx.device.Address,
-		iface, currentCellID, primaryband, caband)
 
 	return nil
+}
+
+func convertFromLteStatus(value string) (float64, error) {
+	if value == "connected" {
+		return 1.0, nil
+	}
+
+	return 0.0, nil
 }
