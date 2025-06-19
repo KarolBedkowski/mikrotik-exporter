a7794690d1267565385976bead0851240818a432
Karol Będkowski <karol.bedkowski@gmail.com>
2025-06-19 09:45:42 +0200

    feat: add collector for /ip/dns/adlist

---
+++ collectors/dns_adlist_collector.go
@@ -0,0 +1,57 @@
+package collectors
+
+//
+// Get metrics from /dns/adlist - number of entries and total match for each adlist.
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func init() {
+	registerCollector("dns_adlist", newDNSAdlistCollector,
+		"retrieves dns adlist metrics")
+}
+
+type dnsAdlistCollector struct {
+	metrics PropertyMetricList
+}
+
+func newDNSAdlistCollector() RouterOSCollector {
+	const prefix = "dns_adlist"
+
+	labelNames := []string{"name", "address", "url"}
+
+	collector := &dnsAdlistCollector{
+		metrics: PropertyMetricList{
+			NewPropertyCounterMetric(prefix, "match-count", labelNames).WithName("match_count_total").Build(),
+			NewPropertyGaugeMetric(prefix, "name-count", labelNames).WithName("name_count").Build(),
+		},
+	}
+
+	return collector
+}
+
+func (c *dnsAdlistCollector) Describe(ch chan<- *prometheus.Desc) {
+	c.metrics.Describe(ch)
+}
+
+func (c *dnsAdlistCollector) Collect(ctx *CollectorContext) error {
+	reply, err := ctx.client.Run("/ip/dns/adlist/print", "=.proplist=url,match-count,name-count")
+	if err != nil {
+		return fmt.Errorf("fetch dns adlist stats error: %w", err)
+	}
+
+	var errs *multierror.Error
+
+	for _, re := range reply.Re {
+		lctx := ctx.withLabelsFromMap(re.Map, "url")
+		if err := c.metrics.Collect(re, &lctx); err != nil {
+			errs = multierror.Append(errs, fmt.Errorf("collect error: %w", err))
+		}
+	}
+
+	return errs.ErrorOrNil()
+}
