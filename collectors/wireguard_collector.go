979f20ca97601e50851d08a708401ce811fc82e6
Karol Będkowski <karol.bedkowski@gmail.com>
2025-06-20 09:51:11 +0200

    feat: collect running status for wireguard

--- collectors/wireguard_collector.go
+++ collectors/wireguard_collector.go
@@ -14,18 +14,23 @@ func init() {
 
 type wireguardCollector struct {
 	metrics PropertyMetricList
+	wg      PropertyMetricList
 }
 
 func newWireguardCollector() RouterOSCollector {
 	const prefix = "wireguard"
 
 	labelNames := []string{"name", "address", "public_key", "comment", "disabled"}
+	wgLabelNames := []string{"name", "address", "public_key", "wg_name", "comment"}
 
 	return &wireguardCollector{
 		metrics: PropertyMetricList{
 			NewPropertyGaugeMetric(prefix, "last-handshake", labelNames).WithConverter(metricFromDuration).Build(),
 			NewPropertyCounterMetric(prefix, "rx", labelNames).Build(),
 			NewPropertyCounterMetric(prefix, "tx", labelNames).Build(),
+		},
+		wg: PropertyMetricList{
+			NewPropertyGaugeMetric(prefix, "running", wgLabelNames).WithConverter(metricFromBool).Build(),
 		},
 	}
 }
@@ -52,6 +57,23 @@ func (c *wireguardCollector) Collect(ctx *CollectorContext) error {
 		lctx := ctx.withLabels(pubKey, re.Map["comment"], re.Map["disabled"])
 
 		if err := c.metrics.Collect(re, &lctx); err != nil {
+			errs = multierror.Append(errs,
+				fmt.Errorf("collect wireguard %v error: %w", re, err))
+		}
+	}
+
+	reply, err = ctx.client.Run("/interface/wireguard/print",
+		"=.proplist=comment,public-key,comment,disabled,running,name")
+	if err != nil {
+		errs = multierror.Append(fmt.Errorf("fetch wireguard status error: %w", err))
+
+		return errs.ErrorOrNil()
+	}
+
+	for _, re := range reply.Re {
+		lctx := ctx.withLabelsFromMap(re.Map, "public-key", "name", "comment")
+
+		if err := c.wg.Collect(re, &lctx); err != nil {
 			errs = multierror.Append(errs,
 				fmt.Errorf("collect wireguard %v error: %w", re, err))
 		}
