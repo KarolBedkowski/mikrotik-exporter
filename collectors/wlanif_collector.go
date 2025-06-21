98f501fe6dbd4bac938e925e031f36e9e5e69070
Karol Będkowski <karol.bedkowski@gmail.com>
2025-06-21 10:27:16 +0200

    fix: wifiif - collect disabled interfaces managed by capsman

--- collectors/wlanif_collector.go
+++ collectors/wlanif_collector.go
@@ -46,27 +46,23 @@ func (c *wlanIFCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (c *wlanIFCollector) Collect(ctx *CollectorContext) error {
-	names, err := c.fetchInterfaceNames(ctx)
+	reply, err := ctx.client.Run("/interface/wireless/print", "=.proplist=name,disabled,frequency")
 	if err != nil {
-		return err
+		return fmt.Errorf("fetch wireless error: %w", err)
 	}
 
-	for _, n := range names {
-		if err := c.collectForInterface(n, ctx); err != nil {
+	for _, re := range reply.Re {
+		// skip disabled interfaces without frequency; if there is frequeny - interface is managed by capsman
+		if re.Map["disabled"] == "true" && re.Map["frequency"] == "" {
+			continue
+		}
+
+		if err := c.collectForInterface(re.Map["name"], ctx); err != nil {
 			return err
 		}
 	}
 
 	return nil
-}
-
-func (c *wlanIFCollector) fetchInterfaceNames(ctx *CollectorContext) ([]string, error) {
-	reply, err := ctx.client.Run("/interface/wireless/print", "?disabled=false", "=.proplist=name")
-	if err != nil {
-		return nil, fmt.Errorf("fetch wireless error: %w", err)
-	}
-
-	return extractPropertyFromReplay(reply, "name"), nil
 }
 
 func (c *wlanIFCollector) collectForInterface(iface string, ctx *CollectorContext) error {
@@ -110,7 +106,7 @@ func (c *wlanIFCollector) collectMetricForFreq(iface string, re *proto.Sentence,
 
 		value, err := strconv.ParseFloat(freq, 64)
 		if err != nil {
-			return fmt.Errorf("collect channel for %s perse %v error: %w", iface, freq, err)
+			return fmt.Errorf("collect channel for %s parse %v error: %w", iface, freq, err)
 		}
 
 		ctx.ch <- prometheus.MustNewConstMetric(c.frequencyDesc, prometheus.GaugeValue,
